5edc2b36ac6cb85a3fdd93ba7229796b5c284436
guilhermeangui <guilhermeangui@live.com>
2025-01-30 02:57:15 -0300

    :pencil: fixing eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,9 +61,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
 		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 	  `;
 	} catch (error) {
-	  // If a database error occurs, return a more specific error.
 	  return {
-		message: 'Database Error: Failed to Create Invoice.',
+		message: `Database Error: Failed to Create Invoice. ${error}`,
 	  };
 	}
    
@@ -100,7 +99,7 @@ export async function updateInvoice(
 		WHERE id = ${id}
 	  `;
 	} catch (error) {
-	  return { message: 'Database Error: Failed to Update Invoice.' };
+	  return { message: `Database Error: Failed to Update Invoice. ${error}` };
 	}
    
 	revalidatePath('/dashboard/invoices');
